9449a0eb3e8f14ecb4c3b78d32d5da3acc7dfbd2
chimbiwide <david_wang28@outlook.com>
2026-08-24 22:07:09 -0400

    feat: added sync script

---
+++ sync.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Bash
+cp ./.bashrc ~/.bashrc
+source ~/.bashrc
+echo "bash synced"
+
+# Kitty
+cp -r ./kitty/ ~/.config/kitty/
+if pgrep -x kitty >/dev/null; then
+    pkill -USR1 -x kitty
+    echo "kitty synced"
+else
+    echo "No kitty instances"
+fi
+
+# neovim
+cp ./init.lua ~/.config/nvim/init.lua
+echo "nvim synced"
